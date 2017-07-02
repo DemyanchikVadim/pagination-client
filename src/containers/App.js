378d90326a6ca0a27f4236faa8c3a4b69341eff4
vademyanchik <vadim.demyanchik@gmail.com>
2017-07-02 20:42:57 +0300

    Test commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-l" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Vadim</h2>
         </div>
         <p className="App-intrfdsfsdo">
         </p>
